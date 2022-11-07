4395af0f6cf6ac91d9c98748afadb37d020fe30e
chengchengchenchen <97878242+chengchengchenchen@users.noreply.github.com>
2022-11-07 20:50:17 +0800

    Add files via upload

---
+++ contracts/contracts/StudentERC721.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+contract GameItem is ERC721 {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    mapping (uint256 => string) private _tokenURIs;
+    constructor() public ERC721("GameItem", "ITM"){
+    }
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function awardItem(address player, string memory tokenURI) public returns(uint256){
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+}
