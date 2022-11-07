9413cd27f3207db079ab0c0483d0e0ec9adb57ea
chengchengchenchen <97878242+chengchengchenchen@users.noreply.github.com>
2022-11-07 20:50:56 +0800

    Update deploy.ts

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -5,7 +5,22 @@ async function main() {
   const studentSocietyDAO = await StudentSocietyDAO.deploy();
   await studentSocietyDAO.deployed();
 
-  console.log(`StudentSocietyDAO deployed to ${studentSocietyDAO.address}`);
+    console.log(`StudentSocietyDAO deployed to ${studentSocietyDAO.address}`);
+
+    const Erc20 = await ethers.getContractFactory("StudentERC20");
+    const erc20 = await Erc20.deploy("ZJUToken", "ZJUTokenSymbol");
+    await erc20.deployed();
+
+    console.log(`ERC20 deployed to ${erc20.address}`);
+
+    const test = await studentSocietyDAO.studentERC20()
+    console.log(`erc20 contract has been deployed successfully in ${test}`)
+
+    const Erc721 = await ethers.getContractFactory("GameItem");
+    const erc721 = await Erc721.deploy();
+    await erc721.deployed();
+
+    console.log(`ERC721 deployed to ${erc721.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
